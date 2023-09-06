ee8e652819641a4b7b16e78f5b80f070b5883c56
MrBeast69420 <124798741+MrBeast69420@users.noreply.github.com>
2023-09-06 09:21:59 +0200

    Update Quest.cs
    quest accepten af

--- Quest.cs
+++ Quest.cs
@@ -1,3 +1,35 @@
-public static class Quest{
-  
+namespace Game;
+namespace Game;
+
+class Quest
+{
+    public int ID;
+
+    public string Name;
+    public string Description;
+
+    public int RewardExperiencePoints;
+    public int RewardGold;
+
+    public Item RewardItem;
+    public Weapon RewardWeapon;
+
+    public CountedItemList QuestCompletionItems = new CountedItemList();
+
+    public Quest(int id, string name, string description, int rewardExperiencePoints, int rewardGold, Item rewardItem,
+        Weapon rewardWeapon)
+    {
+        this.ID = id;
+        this.Name = name;
+        this.Description = description;
+        this.RewardExperiencePoints = rewardExperiencePoints;
+        this.RewardGold = rewardGold;
+        this.RewardItem = rewardItem;
+        this.RewardWeapon = rewardWeapon;
+    }
+    
+    public void ShowDialog()
+    {
+        Console.WriteLine($"There is a quest available: {Name}\n{Description}");
+    }
 }
