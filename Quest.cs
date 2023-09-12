9755e5b25c9165a0b3b3c8693d46191b33d6bdf0
Swifty-Dog <70727050+Swifty-Dog@users.noreply.github.com>
2023-09-12 16:23:39 +0200

    Update Quest.cs

--- Quest.cs
+++ Quest.cs
@@ -1,92 +1,91 @@
-public class Quest
+using System;
+
+namespace Game
 {
-    public int ID;
-
-    public string Name;
-    public string Description;
-
-    public int RewardExperiencePoints;
-    public int RewardGold;
-
-    public Item RewardItem;
-    public Weapon RewardWeapon;
-
-    public CountedItemList QuestCompletionItems = new CountedItemList();
-}
-
-    public Quest(int id, string name, string description, int rewardExperiencePoints, int rewardGold, Item rewardItem,
-        Weapon rewardWeapon)
+    public class Quest
     {
-        this.ID = id;
-        this.Name = name;
-        this.Description = description;
-        this.RewardExperiencePoints = rewardExperiencePoints;
-        this.RewardGold = rewardGold;
-        this.RewardItem = rewardItem;
-        this.RewardWeapon = rewardWeapon;
+        public int ID;
+
+        public string Name;
+        public string Description;
+
+        public int RewardExperiencePoints;
+        public int RewardGold;
+        public Weapon RewardWeapon;
+
     }
-    
-    public void ShowDialog()
-    {
-        Console.WriteLine($"There is a quest available: {Name}\n{Description}");
-    }
-    public void CompleteQuest()
-    {
-        if (Player.CurrentLocation.QuestAvailableHere == .CurrentQuest && Player.CurrentQuest != null)
+
+        public Quest(int id, string name, string description, int rewardExperiencePoints, int rewardGold,
+            Weapon rewardWeapon)
         {
-            Quest quest = Player.CurrentQuest;
-            bool checking = true;
-            while (checking)
+            this.ID = id;
+            this.Name = name;
+            this.Description = description;
+            this.RewardWeapon = rewardWeapon;
+        }
+        
+        public void ShowDialog()
+        {
+            Console.WriteLine($"There is a quest available: {Name}\n{Description}");
+        }
+        public void CompleteQuest()
+        {
+            if (Player.CurrentLocation.QuestAvailableHere == Player.CurrentQuest && Player.CurrentQuest != null)
             {
+                Quest quest = Player.CurrentQuest;
+                bool checking = true;
+                while (checking)
                 {
-                    bool completed = false;
-                    foreach (Item item in Player.Inventory)
                     {
-                        if (item == quest.ItemToCollect && item.Amount >= quest.AmountToCollect)
+                        bool completed = false;
+                        foreach (Item item in Player.Inventory)
+                        {
+                            if (item == quest.ItemToCollect && item.Amount >= quest.AmountToCollect)
+                            {
+                                Console.WriteLine("-------------------------------------------------------------------");
+                                Console.WriteLine($"Congrats! You have collected enough {quest.ItemToCollect.ItemName()}.");                      
+                                Console.WriteLine("Take this as a token of gratitude.");
+                                if (quest.Item == null)
+                                {
+                                    Console.WriteLine("-------------------------------------------------------------------");
+                                    Console.WriteLine($"You have received: {quest.Weapon.Name}");
+                                    Player.EquippedWeapon = quest.Weapon;
+                                    completed = true;
+                                    Player.CurrentQuest = null;
+                                    World.Quests.Remove(quest);
+                                    Player.CurrentLocation.QuestAvailableHere = null;
+                                    break;
+                                }
+                                else
+                                {
+                                    Console.WriteLine("-------------------------------------------------------------------");
+                                    Console.WriteLine($"*you have received: {quest.Item.SingleName}.");
+                                    Player.GetItem(quest.Item);
+                                    completed = true;
+                                    Player.CurrentQuest = null;
+                                    World.Quests.Remove(quest);
+                                    Player.CurrentLocation.QuestAvailableHere = null;
+                                    break;
+                                }
+                            }
+                        }
+
+                        if (completed)
+                        {
+                            while (Player.Inventory.Contains(quest.ItemToCollect))
+                            {
+                                Player.RemoveItem(quest.ItemToCollect);
+                            }
+                        }
+
+                        else
                         {
                             Console.WriteLine("-------------------------------------------------------------------");
-                            Console.WriteLine($"Congrats! You have collected enough {quest.ItemToCollect.ItemName()}.");                      
-                            Console.WriteLine("Take this as a token of gratitude.");
-                            if (quest.Item == null)
-                            {
-                                Console.WriteLine("-------------------------------------------------------------------");
-                                Console.WriteLine($"You have received: {quest.Weapon.Name}");
-                                Player.EquippedWeapon = quest.Weapon;
-                                completed = true;
-                                Player.CurrentQuest = null;
-                                World.Quests.Remove(quest);
-                                Player.CurrentLocation.QuestAvailableHere = null;
-                                break;
-                            }
-                            else
-                            {
-                                Console.WriteLine("-------------------------------------------------------------------");
-                                Console.WriteLine($"*you have received: {quest.Item.SingleName}.");
-                                Player.GetItem(quest.Item);
-                                completed = true;
-                                Player.CurrentQuest = null;
-                                World.Quests.Remove(quest);
-                                Player.CurrentLocation.QuestAvailableHere = null;
-                                break;
-                            }
+                            Console.WriteLine($"This is not enough. Return when you have {quest.AmountToCollect} {quest.ItemToCollect.ItemName()}");
                         }
+                        checking = false;
                     }
-
-                    if (completed)
-                    {
-                        while (Player.Inventory.Contains(quest.ItemToCollect))
-                        {
-                            Player.RemoveItem(quest.ItemToCollect);
-                        }
-                    }
-
-                    else
-                    {
-                        Console.WriteLine("-------------------------------------------------------------------");
-                        Console.WriteLine($"This is not enough. Return when you have {quest.AmountToCollect} {quest.ItemToCollect.ItemName()}");
-                    }
-                    checking = false;
                 }
             }
-        }
+    }
 }
