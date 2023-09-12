975739038e9a4ce30534b081d5e51be299f001b6
Swifty-Dog <70727050+Swifty-Dog@users.noreply.github.com>
2023-09-12 19:27:13 +0200

    Update Quest.cs

--- Quest.cs
+++ Quest.cs
@@ -1,35 +1,23 @@
-namespace Game;
-namespace Game;
+using System.Linq;
 
 class Quest
 {
     public int ID;
-
     public string Name;
     public string Description;
+    public Item? Item;
+    public Weapon? Weapon;
+    public Item ItemToCollect;
+    public int AmountToCollect;
 
-    public int RewardExperiencePoints;
-    public int RewardGold;
-
-    public Item RewardItem;
-    public Weapon RewardWeapon;
-
-    public CountedItemList QuestCompletionItems = new CountedItemList();
-
-    public Quest(int id, string name, string description, int rewardExperiencePoints, int rewardGold, Item rewardItem,
-        Weapon rewardWeapon)
+    public Quest(int id, string name, string description, Item? item, Weapon? weapon, Item? itemToCollect, int amountToCollect)
     {
-        this.ID = id;
-        this.Name = name;
-        this.Description = description;
-        this.RewardExperiencePoints = rewardExperiencePoints;
-        this.RewardGold = rewardGold;
-        this.RewardItem = rewardItem;
-        this.RewardWeapon = rewardWeapon;
-    }
-    
-    public void ShowDialog()
-    {
-        Console.WriteLine($"There is a quest available: {Name}\n{Description}");
+        ID = id;
+        Name = name;
+        Description = description;
+        Item = item;
+        Weapon = weapon;
+        ItemToCollect = itemToCollect;
+        AmountToCollect = amountToCollect;
     }
 }
