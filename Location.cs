78574285f4229cc89a49a2c42d14c79fa05ff560
Swifty-Dog <70727050+Swifty-Dog@users.noreply.github.com>
2023-09-12 16:38:00 +0200

    Update Location.cs

--- Location.cs
+++ Location.cs
@@ -11,4 +11,55 @@ public class Location
         Name = name;
         Description = description;
     }
+    public Location GetLocationAt(string direction)
+        {
+            switch (direction)
+            {
+                case "N":
+                    return LocationToNorth;
+                case "E":
+                    return LocationToEast;
+                case "S":
+                    return LocationToSouth;
+                case "W":
+                    return LocationToWest;
+                default:
+                    return null;
+            }
+        }
+
+        public string Compass()
+        {
+            string compass = "";
+            if (LocationToNorth != null)
+            {
+                compass += "    N\n    |\n";
+            }
+
+            if (LocationToWest != null)
+            {
+                compass += "W---|";
+            }
+            else
+            {
+                compass += "    |";
+            }
+            
+            if (LocationToEast != null)
+            {
+                compass += "---E\n";
+            }
+            else
+            {
+                compass += "\n";
+            }
+            if (LocationToSouth != null)
+            {
+                compass += "    |\n    S";
+            }
+
+            return compass;
+        }
+    }
+}
 }
